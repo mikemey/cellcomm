44813efece0f0b99130cfe1c727179c00fe7fe80
mikemey <miguel.maestro@gmx.co.uk>
2020-06-28 21:26:48 +0100

    implement cell-type generation

--- src/cell_generator.py
+++ src/cell_generator.py
@@ -1,15 +1,46 @@
+from functools import reduce
+
+from numpy import random
+
 MAX_TRANSCRIPTS = pow(26, 3)
 
-transcripts_range = range(65, 91)
 
-
-def generate_transcripts(count):
+def generate_transcript_labels(count):
     if count > MAX_TRANSCRIPTS:
         raise AssertionError(f'too many transcripts: {count}')
     tcs = []
-    for i in transcripts_range:
-        for j in transcripts_range:
-            for k in transcripts_range:
+    char_range = range(65, 91)
+    for i in char_range:
+        for j in char_range:
+            for k in char_range:
                 tcs.append(f'{i:c}{j:c}{k:c}')
                 if len(tcs) == count:
                     return tcs
+
+
+class CellType:
+    def __init__(self, name, t_dists):
+        self.name = name
+        self.transcript_dists = t_dists
+
+    def __str__(self):
+        dists = reduce(lambda all, ts: f'{all}\n\t{ts}', self.transcript_dists)
+        return f'Cell type "{self.name}":\n{dists}'
+
+
+class CellTranscript:
+    def __init__(self, label, mean, sd):
+        self.label = label
+        self.mean = mean
+        self.sd = sd
+
+    def __str__(self):
+        return f'Transcript "{self.label}" mean: {self.mean}, std-dev: {self.sd}'
+
+
+def generate_cell_type(type_name, tc_labels, expressed_ratio, dist_range, dist_variance):
+    expressed_size = int(len(tc_labels) * expressed_ratio)
+    selected = random.choice(tc_labels, expressed_size, replace=False)
+    tc_dists = [CellTranscript(label, random.randint(*dist_range), random.randint(*dist_variance))
+                for label in selected]
+    return CellType(type_name, tc_dists)
--- test/cell_generator_test.py
+++ test/cell_generator_test.py
@@ -1,22 +1,40 @@
 import unittest
 
-import cell_generator as data_gen
+import numpy as np
+
+import cell_generator as cg
 
 
-class DataGenTest(unittest.TestCase):
-    def test_generate_small_transcripts(self):
-        tcs = data_gen.generate_transcripts(4)
+class CellGenTest(unittest.TestCase):
+    def test_generate_small_transcript_labels(self):
+        tcs = cg.generate_transcript_labels(4)
         self.assertListEqual(['AAA', 'AAB', 'AAC', 'AAD'], tcs)
 
-    def test_generate_large_transcripts(self):
+    def test_generate_large_transcript_labels(self):
         count = 2000
-        tcs = data_gen.generate_transcripts(count)
+        tcs = cg.generate_transcript_labels(count)
         self.assertEqual(count, len(tcs))
         self.assertEqual('AAA', tcs[0])
         self.assertEqual('CYX', tcs[count - 1])
 
-    def test_reject_too_many_transcripts(self):
+    def test_reject_too_many_transcript_labels(self):
         too_many = pow(26, 3) + 1
         with self.assertRaises(AssertionError) as ctx:
-            data_gen.generate_transcripts(too_many)
+            cg.generate_transcript_labels(too_many)
         self.assertEqual(f'too many transcripts: {too_many}', str(ctx.exception))
+
+    def test_generate_cell_type(self):
+        np.random.seed(0)
+        tcs = cg.generate_transcript_labels(5)
+        name = 'test-type'
+
+        ctype = cg.generate_cell_type(
+            name, tcs, expressed_ratio=0.6, dist_range=(10, 10000), dist_variance=(10, 400)
+        )
+        self.assertEqual(name, ctype.name)
+        self.__assert_cell_transcript(ctype.transcript_dists[0], 4869, 205)
+        self.__assert_cell_transcript(ctype.transcript_dists[1], 9235, 221)
+
+    def __assert_cell_transcript(self, transcript, mean, sd):
+        self.assertEqual(transcript.mean, mean)
+        self.assertEqual(transcript.sd, sd)
