aef9fbf4b79e51089f6a721d20609dff59ba9569
mikemey <miguel.maestro@gmx.co.uk>
2020-07-05 21:16:34 +0100

    add cell-data generator model

--- src/cell_type_training.py
+++ src/cell_type_training.py
@@ -1,6 +1,8 @@
 import os
 
 import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import Model, layers
 
 CELL_MATRIX_FILE = os.path.join(os.path.dirname(__file__),
                                 '..', 'data', 'GSE122930_Sham_1_week_matrix.mtx')
@@ -10,6 +12,22 @@ def load_matrix(file):
     df = pd.read_csv(file, header=None, skiprows=3,
                      delim_whitespace=True, names=['gene', 'barcode', 'p'])
     return df.pivot_table(index='barcode', columns='gene', values='p', fill_value=0)
+
+
+def _build_generator(encoding_shape, gene_count):
+    encoding_in = layers.Input(encoding_shape)
+    x = layers.Dense(encoding_shape[0] * 10, activation=tf.nn.relu)(encoding_in)
+    data_out = layers.Dense(gene_count, activation=tf.nn.relu)(x)
+    return Model(encoding_in, data_out, name='Cell generator')
+
+
+class CellBigan:
+    def __init__(self, encoding_shape, gene_count):
+        self._generator = _build_generator(encoding_shape, gene_count)
+        self.summary()
+
+    def summary(self):
+        self._generator.summary()
 
 
 class CellTraining:
--- test/cell_type_training_test.py
+++ test/cell_type_training_test.py
@@ -4,10 +4,13 @@ import unittest
 import numpy as np
 import tensorflow as tf
 
-from cell_type_training import CellTraining, load_matrix
+from cell_type_training import load_matrix, CellTraining, CellBigan
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
 
 TEST_TRAINING_FILE = os.path.join(os.path.dirname(__file__), 'example_matrix.mtx')
 TEST_BATCH_SIZE = 3
+TEST_GENE_COUNT = 11
 TEST_MATRIX_CONTENT = [
     # gene: 32, 34, 39, 40, 60, 63, 23764, 27918, 27919, 27921, 27994
     [0, 0, 0, 0, 1, 11, 0, 0, 0, 6, 1],
@@ -19,17 +22,15 @@ TEST_MATRIX_CONTENT = [
 
 
 class TrainingTestCase(unittest.TestCase):
-    def setUp(self):
-        self.cell_training = CellTraining(TEST_TRAINING_FILE, TEST_BATCH_SIZE)
-
     def test_load_matrix_and_pivot(self):
         cell_batch = load_matrix(TEST_TRAINING_FILE)
-        self.assertEqual((5, 11), cell_batch.shape)
+        self.assertEqual((5, TEST_GENE_COUNT), cell_batch.shape)
         self.__assert_data(TEST_MATRIX_CONTENT, cell_batch)
 
     def test_sample_cell_data(self):
-        sampled = self.cell_training.sample_cell_data(0)
-        self.assertEqual((TEST_BATCH_SIZE, 11), sampled.shape)
+        cell_training = CellTraining(TEST_TRAINING_FILE, TEST_BATCH_SIZE)
+        sampled = cell_training.sample_cell_data(0)
+        self.assertEqual((TEST_BATCH_SIZE, TEST_GENE_COUNT), sampled.shape)
         self.__assert_data([TEST_MATRIX_CONTENT[2],
                             TEST_MATRIX_CONTENT[0],
                             TEST_MATRIX_CONTENT[1]], sampled)
@@ -37,3 +38,10 @@ class TrainingTestCase(unittest.TestCase):
     def __assert_data(self, expected, actual):
         eq_matrix = tf.math.equal(expected, actual)
         self.assertTrue(np.all(eq_matrix), f'\tExpected:\n{expected}\n\tActual:\n{actual}')
+
+    def test_generator_model(self):
+        cell_bigan = CellBigan(encoding_shape=(15,), gene_count=1000)
+        generator = cell_bigan._generator
+        self.assertEqual((None, 15), generator.input_shape)
+        self.assertEqual((None, 1000), generator.output_shape)
+        self.assertEqual(tf.nn.relu, generator.layers[-1].activation)
