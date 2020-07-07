05040c02e8f559e26e9b0fd02a8659fce74be37a
mikemey <miguel.maestro@gmx.co.uk>
2020-07-07 18:37:03 +0100

    refactor to consume losses-log-files

--- analysis.py
+++ analysis.py
@@ -1,20 +1,14 @@
-import re
-
 import matplotlib.pyplot as plt
 
-with open('logs/first.txt') as f:
+with open('logs/07-07-long-run/losses.csv') as f:
     xs = []
     all_loss_graphs = [[], [], [], []]
-    for line in f.readlines():
-        def interested_cols(token):
-            return len(token) > 0 and not re.search('[a-zA-Z]', token)
-
-
+    for line in f.readlines()[1:]:
         def clean(token):
             return float(token.strip())
 
 
-        tokens = map(clean, filter(interested_cols, line.split(' ')))
+        tokens = map(clean, line.split(','))
         it, total_loss, g_loss, e_loss, d_loss = list(tokens)
         xs.append(it)
         all_loss_graphs[0].append(total_loss)
@@ -29,16 +23,16 @@ plt.rc('xtick.major', width=0.2)
 plt.rc('ytick.major', width=0.2)
 
 fig = plt.figure(figsize=(4, 2.2), dpi=400)
-plt.grid(True, linewidth=0.2)
 
 plt.plot(xs, all_loss_graphs[0], '-k', label='total')
 plt.plot(xs, all_loss_graphs[1], '-b', label='generator')
 plt.plot(xs, all_loss_graphs[2], '-g', label='encoder')
 plt.plot(xs, all_loss_graphs[3], '-r', label='discriminator')
-plt.xlim(xmin=0, xmax=max(xs) + 1)
-plt.ylim(0)
-legend_frame = plt.legend(title='Losses', framealpha=1.0).get_frame()
 
+plt.grid(True, linewidth=0.2)
+plt.xlim(xmin=0, xmax=max(xs) + 1)
+plt.ylim(ymin=0)
+legend_frame = plt.legend(title='Losses', framealpha=1.0).get_frame()
 legend_frame.set_linewidth(0.4)
 
 fig.tight_layout()
