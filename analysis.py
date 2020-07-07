01403bfa96ba8d87a735109ea97650de730d476f
mikemey <miguel.maestro@gmx.co.uk>
2020-07-07 18:06:18 +0100

    create/save losses-graph

---
+++ analysis.py
@@ -0,0 +1,47 @@
+import re
+
+import matplotlib.pyplot as plt
+
+with open('logs/first.txt') as f:
+    xs = []
+    all_loss_graphs = [[], [], [], []]
+    for line in f.readlines():
+        def interested_cols(token):
+            return len(token) > 0 and not re.search('[a-zA-Z]', token)
+
+
+        def clean(token):
+            return float(token.strip())
+
+
+        tokens = map(clean, filter(interested_cols, line.split(' ')))
+        it, total_loss, g_loss, e_loss, d_loss = list(tokens)
+        xs.append(it)
+        all_loss_graphs[0].append(total_loss)
+        all_loss_graphs[1].append(g_loss)
+        all_loss_graphs[2].append(e_loss)
+        all_loss_graphs[3].append(d_loss)
+
+plt.rc('font', size=3)
+plt.rc('lines', lw=0.2)
+plt.rc('axes', lw=0.25)
+plt.rc('xtick.major', width=0.2)
+plt.rc('ytick.major', width=0.2)
+
+fig = plt.figure(figsize=(4, 2.2), dpi=400)
+plt.grid(True, linewidth=0.2)
+
+plt.plot(xs, all_loss_graphs[0], '-k', label='total')
+plt.plot(xs, all_loss_graphs[1], '-b', label='generator')
+plt.plot(xs, all_loss_graphs[2], '-g', label='encoder')
+plt.plot(xs, all_loss_graphs[3], '-r', label='discriminator')
+plt.xlim(xmin=0, xmax=max(xs) + 1)
+plt.ylim(0)
+legend_frame = plt.legend(title='Losses', framealpha=1.0).get_frame()
+
+legend_frame.set_linewidth(0.4)
+
+fig.tight_layout()
+fig.savefig('test.png')
+plt.show()
+plt.close(fig)
