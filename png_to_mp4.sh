83c659db8383278b84111182e7a99ade738ee526
mikemey <miguel.maestro@gmx.co.uk>
2020-07-13 19:58:50 +0100

    refactor converting to function, use for distinct img-ids in different runs

--- png_to_mp4.sh
+++ png_to_mp4.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 
-run_ids=(
-  "07-10-1809-TAC4-enc13"
-  "07-10-2042-TAC4-enc14"
-  "07-10-2317-TAC4-enc15"
-  "07-11-0152-TAC4-enc16"
-  "07-11-0426-TAC4-enc17"
-)
-
 image_delay=50
 image_ids=(
-  "tsne_3d"
-  "umap_pos0"
-  "umap_pos1"
-  "umap_pos2"
+  "2Dc"
+  "3Dm"
 #  "pca_3d"
 #  "pca_4d"
 #  "umap_2d"
@@ -21,21 +11,32 @@ image_ids=(
 #  "umap_4d"
 )
 
-for run_id in ${run_ids[@]}; do
-  printf "\t============ run id: '${run_id}' ============\n"
-  for iid in ${image_ids[@]}; do
-    files=`ls -1 logs/${run_id}/*${iid}*`
-    fl_file="z_${iid}.txt"
-    gif_file="z_${iid}.gif"
-    mp4_file="${run_id}_${iid}.mp4"
+function convert_to_mp4 () {
+    run_id="$1"
+    unique_img_id="$2"
+    printf "\t============ converting: \"${run_id}\"\n"
+    files=`ls -1 logs/${run_id}/*${unique_img_id}*`
+    fl_file="z_${unique_img_id}.txt"
+    gif_file="z_${unique_img_id}.gif"
+    mp4_file="${run_id}_${unique_img_id}.mp4"
 
     echo "$files" > "$fl_file"
 
-    printf "image id '${iid}' = converting to gif... "
+    printf "image id '${unique_img_id}' = converting to gif... "
     convert -delay ${image_delay} @${fl_file} ${gif_file}
     printf "to mp4...\n"
     HandBrakeCLI --preset="HQ 720p30 Surround" --audio "none" -i "$gif_file" -o "$mp4_file" 2> /dev/null
     rm ${fl_file}
     rm ${gif_file}
-  done
-done
+}
+
+convert_to_mp4 "07-13-1954-TAC4-de_3" "2Dc"
+convert_to_mp4 "07-13-1954-TAC4-de_3" "3Dm"
+convert_to_mp4 "07-13-1954-TAC4-de_4" "3Dc"
+
+#for run_id in ${@}; do
+#  printf "\t============ run id: '${run_id}' ============\n"
+#  for iid in ${image_ids[@]}; do
+#    convert_to_mp4 "$run_id" "$iid"
+#  done
+#done
